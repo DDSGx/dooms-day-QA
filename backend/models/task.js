9375ed8a04ae95182cc6b8610f9646beeb921d27
s-tyd <shoyatoyoda@gmail.com>
2020-04-03 23:29:31 +0900

    src push

---
+++ backend/models/task.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const task = sequelize.define('task', {
+    taskname: DataTypes.STRING
+  }, {
+    underscored: true,
+  });
+  task.associate = function(models) {
+    // associations can be defined here
+  };
+  return task;
+};
