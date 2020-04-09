3948351781638a3ac9076de323fc00ab3d2ec4c8
s-tyd <shoyatoyoda@gmail.com>
2020-04-09 23:26:24 +0900

    header-and-sidemenu

--- client/src/components/NavigationDrawers.vue
+++ client/src/components/NavigationDrawers.vue
@@ -1,18 +1,21 @@
 <template>
-  <v-container class="navigation">
-    <v-navigation-drawer clipped left app>
+  <v-container>
+
+    <v-app-bar color="primary" dark app clippedLeft>
+      <v-app-bar-nav-icon @click="drawer=!drawer" ></v-app-bar-nav-icon>
+      <v-toolbar-title>Dooms Day QA</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer :mini-variant="drawer" clipped left app>
       <v-list dense nav class="py-0">
-        <v-list-item two-line :class="miniVariant && 'px-0'">
+        <v-list-item-content />
+
+        <v-list-item v-for="item in items" :key="item.title" :to="item.link" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>dooms day QA</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-divider></v-divider>
-
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-content class="navigation__content">
-            <v-list-item :to="item.link">{{ item.title }}</v-list-item>
+            <v-list-item-title >{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -25,10 +28,11 @@ export default {
   name: "navigation-drawers",
   data() {
       return {
+          drawer: true,
           items: [
-              {title: 'QATable', link: '/'},
-              {title: 'Logs', link: '/logs'},
-              {title: 'About', link: '/about'},
+              {title: 'QATable', link: '/', icon: 'dashboard'},
+              {title: 'Logs', link: '/logs', icon: 'account_tree'},
+              {title: 'About', link: '/about', icon: 'help'},
           ],
       }
   },
--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,11 +1,14 @@
 // src/plugins/vuetify.js
 
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify);
 
-const opts = {};
-
-export default new Vuetify(opts)
+export default new Vuetify({
+    icons: {
+        iconfont: 'md', // default - only for display purposes
+    },
+})
